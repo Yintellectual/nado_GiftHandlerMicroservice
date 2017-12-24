831e09f25981bf54537ef5074c717d54ff88ca8b
Yu Chen <intellectual.y@gmail.com>
2017-12-24 18:39:42 +0800

    init

---
+++ src/main/java/com/nado/GiftHandlerMicroservice/GiftHandlerMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.nado.GiftHandlerMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftHandlerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftHandlerMicroserviceApplication.class, args);
+	}
+}
