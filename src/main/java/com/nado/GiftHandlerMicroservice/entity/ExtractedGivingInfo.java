8f0782f2a9a68d344e5198bff2b5253d56577387
Yu Chen <intellectual.y@gmail.com>
2017-12-25 13:03:51 +0800

    update EGI

--- src/main/java/com/nado/GiftHandlerMicroservice/entity/ExtractedGivingInfo.java
+++ src/main/java/com/nado/GiftHandlerMicroservice/entity/ExtractedGivingInfo.java
@@ -9,5 +9,16 @@ public class ExtractedGivingInfo {
 	private String uid;
 	private Integer price;
 	private Long timestamp;
-	private String id;
+	private String messageId;
+	
+	@Override
+	public boolean equals(Object o){
+		if(o!=null && (o instanceof ExtractedGivingInfo)){
+			ExtractedGivingInfo other = (ExtractedGivingInfo) o;
+			return timestamp.equals(other.getTimestamp()) 
+					&& messageId.equals(other.getMessageId());
+		}else{
+			return false;
+		}
+	}
 }
